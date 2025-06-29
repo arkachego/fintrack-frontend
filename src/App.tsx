f6b75a80134b63295361d8a22514f3183ad41276
Arka Halder <contact@arkahalder.com>
2025-06-29 18:08:07 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App: React.FC = () => {
+
+  return (
+    <div>Expense Manager</div>
+  );
+
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// Libraries
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// Styles
+import './index.css'
+
+// App
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
