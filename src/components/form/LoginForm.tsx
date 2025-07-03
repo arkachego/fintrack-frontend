bd69d0ef69b178da58fbcfb35d94bac3e833c958
Arka Halder <contact@arkahalder.com>
2025-07-03 12:45:42 +0530

    feat: Implement LoginForm component for user authentication

---
+++ src/components/form/LoginForm.tsx
@@ -0,0 +1,80 @@
+// Libraries
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Form, Input } from 'antd';
+import { KeyOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons';
+
+// Components
+import SubmitButton from '../button/SubmitButton';
+
+// Hooks
+import { useAppDispatch } from '../../hooks/useRedux';
+
+// Actions
+import { loadState } from "../../slices/globalSlice";
+
+// Utilities
+import { fetchProfile, loginUser } from "../../utilities/request";
+
+const LoginForm: React.FC = () => {
+
+  const [ form ] = Form.useForm();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [ loading, setLoading ] = useState<boolean>(false);
+
+  const handleLogin = async () => {
+    try {
+      setLoading(true);
+      const payload = form.getFieldsValue();
+      await loginUser(payload.email, payload.password);
+      setTimeout(async () => {
+        const response = await fetchProfile();
+        dispatch(loadState(response.data));
+        navigate('/expenses');
+      }, 500);
+    }
+    catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Form
+      form={form}
+      name="expense-form"
+      layout="vertical"
+      variant='filled'
+      autoComplete="off"
+      disabled={loading}
+      validateTrigger={['onBlur']}
+    >
+      <Form.Item name="email" label="Email Address" rules={[{ required: true, max: 100 }]}>
+        <Input
+          disabled={loading}
+          placeholder="Max 100 characters"
+          prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+        />
+      </Form.Item>
+      <Form.Item style={{ marginBottom: 0 }} name="password" label="Login Password" rules={[{ required: true, min: 8, max: 25 }]}>
+        <Input.Password
+          disabled={loading}
+          placeholder="8-25 characters"
+          prefix={<KeyOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+        />
+      </Form.Item>
+      <SubmitButton
+        form={form}
+        loading={loading}
+        onClick={handleLogin}
+      >
+        <LoginOutlined/>
+        Sign-In
+      </SubmitButton>
+    </Form>
+  );
+
+};
+
+export default LoginForm;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,66 +1,15 @@
 // Libraries
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, Card, Input, Space } from 'antd';
-import { KeyOutlined, LoginOutlined, UserOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
 
-// Actions
-import { loadState } from "../slices/globalSlice";
-
-// Hooks
-import { useAppDispatch } from "../hooks/useRedux";
-
-// Utilities
-import { fetchProfile, loginUser } from "../utilities/request";
+// Forms
+import LoginForm from '../components/form/LoginForm';
 
 const LoginPage: React.FC = () => {
 
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const [ loading, setLoading ] = useState<boolean>(false);
-  const [ email, setEmail ] = useState<string>('');
-  const [ password, setPassword ] = useState<string>('');
-
-  const handleLogin = async () => {
-    try {
-      setLoading(true);
-      await loginUser(email, password);
-      setTimeout(async () => {
-        const response = await fetchProfile();
-        dispatch(loadState(response.data));
-        navigate('/expenses');
-      }, 500);
-    }
-    catch (error) {
-      console.error(error);
-      setLoading(false);
-    }
-  };
-
-
   return (
     <div style={{ width: '100%', height: '100%', display: 'flex', alignItems: "center", justifyContent: "center" }}>
-      <Card title="FinTrack: Sign-In" loading={loading} style={{ width: 360 }}>
-        <Space direction="vertical" size={16} style={{ width: '100%' }}>
-          <Input
-            disabled={loading}
-            placeholder="Email Address"
-            prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-          <Input.Password
-            disabled={loading}
-            placeholder="Login Password"
-            prefix={<KeyOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-          />
-          <Button type="primary" block loading={loading} disabled={loading} onClick={handleLogin}>
-            <LoginOutlined/>
-            Sign-In
-          </Button>
-        </Space>
+      <Card title="FinTrack: Sign-In" style={{ width: 360 }}>
+        <LoginForm/>
       </Card>
     </div>
   );
