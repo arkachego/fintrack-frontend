f11339a89276adf287e25d0a0d25a0ab060a3253
Arka Halder <105145575+arkachego@users.noreply.github.com>
2025-07-03 17:40:59 +0530

    Update request.ts

--- src/utilities/request.ts
+++ src/utilities/request.ts
@@ -8,11 +8,17 @@ import type { AnalyticsType } from '../types/AnalyticsType';
 import type { FileUploadType } from '../types/FileUploadType';
 import type { StatusType } from '../types/StatusType';
 
+const headers: any = {
+  'Content-Type': 'application/json',
+};
+const authToken = localStorage.getItem('x-auth-token');
+if (authToken) {
+  headers['Authorization'] = `Bearer ${token}`;
+}
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_SERVER_BASE_URL || 'http://localhost:3000',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  headers,
   withCredentials: true,
 });
 
